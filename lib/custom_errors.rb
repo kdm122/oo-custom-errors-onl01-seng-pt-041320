f2141ed5f6f10b18a537cfa68d6b0c295b987a39
Kyle McLean <kyledmclean@gmail.com>
2020-05-24 18:10:00 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -9,7 +9,11 @@ class Person
 
   def get_married(person)
     self.partner = person
+    if person.class != Person
+      raise PartnerError
+    else
     person.partner = self
+  end
   end
 
   class PartnerError < StandardError
